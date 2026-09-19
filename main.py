7fd848b4f4e50ff3e8620d0b859091242ec9e7a3
emil270797-cmyk <emil270797@gmail.com>
2026-09-19 15:37:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -84,5 +84,5 @@ async def main():
   logging.info("Telegram-бот начал работу! Жду сообщений...")
   await dp.start_polling(bot)
 
-if name == "main":
+if __name__ == "__main__":
   asyncio.run(main())
